7fa4d05b5eb92aed06a57128556a96c4ed1b47a2
Stijn Tintel <stijn@linux-ipv6.be>
2021-11-28 20:45:38 +0200

    Yay, finally managed to print something.
    Lots of WTFs though. What is this voodoo and black magic :(
    
    Signed-off-by: Stijn Tintel <stijn@linux-ipv6.be>

--- asm.s
+++ asm.s
@@ -5,10 +5,23 @@
 
 _doasm:
 doasm:
+	# need to skip .long because it's not a valid instruction
+	# is this the only way of loading in a variable?
+	# also why is this -. voodoo needed and where is it explained properly
+	bl	0f
+	.long	__hwcap-.
+	# this emits difference between the address of __hwcap and the current address
+	# current address is directly after the bl, where the LR now points
+	# wat? welp
+
+0:
+	# read LR value into GPR4
+	mflr	4
+
 	# print to stdout
 	addi	0,0,4		# linux ppc write syscall number to GPR0
 	addi	3,0,1		# first argument (fd = 1) to GPR3
-	addi	4,0,0xff	# second argument (buf = 0xff) to GPR4
+				# second argument is already in GPR4 by mflr
 	addi	5,0,1		# third argument (len = 1 byte) to GPR5
 	sc
 
