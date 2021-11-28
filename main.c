92a62778c5f4625eedcd01fd8616371226daead5
Stijn Tintel <stijn@linux-ipv6.be>
2021-11-28 06:29:47 +0200

    Initial import.
    This code prints the value of musl's __hwcap in hex, and if AltiVec is
    supported or not.
    
    Signed-off-by: Stijn Tintel <stijn@linux-ipv6.be>

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <sys/auxv.h>
+
+#include "main.h"
+
+size_t __hwcap;
+
+int main(void) {
+	__hwcap = getauxval(AT_HWCAP);
+
+	printf("__hwcap: 0x%08zx\n", __hwcap);
+
+	if (__hwcap & PPC_FEATURE_HAS_ALTIVEC)
+		printf("AltiVec supported\n");
+	else
+		printf("AltiVec not supported\n");
+
+	return 0;
+}
---
+++ main.h
@@ -0,0 +1,3 @@
+#define PPC_FEATURE_HAS_ALTIVEC	0x10000000
+
+extern size_t __hwcap;
