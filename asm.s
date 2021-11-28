7e49bee180d0812e8521bd93da336ff1eb12f3ab
Stijn Tintel <stijn@linux-ipv6.be>
2021-11-28 08:17:33 +0200

    problem 1: why does this not print anything at all
    If something simple as this doesn't work, debugging anything becomes
    impossible.
    
    edit: write takes a pointer as second argument, so it tries to access
    memory at 0xff and throws EFAULT (Bad address).
    
    Signed-off-by: Stijn Tintel <stijn@linux-ipv6.be>

--- asm.s
+++ asm.s
@@ -5,6 +5,13 @@
 
 _doasm:
 doasm:
+	# print to stdout
+	addi	0,0,4		# linux ppc write syscall number to GPR0
+	addi	3,0,1		# first argument (fd = 1) to GPR3
+	addi	4,0,0xff	# second argument (buf = 0xff) to GPR4
+	addi	5,0,1		# third argument (len = 1 byte) to GPR5
+	sc
+
 	# exit 0 without mnemonics
 	addi	0,0,1	# linux ppc exit syscall number to GPR0
 	addi	3,0,0	# first argument (status = 0) to GPR3
