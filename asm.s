bef0d39dd4fb4212c80cccc8f1b47f7a18e31dd3
Stijn Tintel <stijn@linux-ipv6.be>
2021-12-06 22:03:26 +0200

    Use different registers for every intermediate result.
    The previous code works fine on Power8, but does not on E5500/E6500.
    After the 2nd lwz, the destination register has value 0 there.
    Experiment a bit to hopefully figure out what is going on.
    
    Signed-off-by: Stijn Tintel <stijn@linux-ipv6.be>

--- asm.s
+++ asm.s
@@ -18,15 +18,15 @@ doasm:
 	# wat? welp
 
 0:
-	# read LR value into GPR3
-	mflr	3
+	# read LR value into GPR10
+	mflr	10
 
 	# voodoo to get actual value of __hwcap
-	lwz	4, 0(3)
-	add	3, 3, 4
-	lwz	3, 0(3)
+	lwz	9, 0(10)
+	add	8, 9, 10
+	lwz	7, 0(8)
 
-	andis.	3, 3, 0x1000
+	andis.	6, 7, 0x1000
 
 	# set retval to false - altivec not supported
 	addi	3, 0, 0
