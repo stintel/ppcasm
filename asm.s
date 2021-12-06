f48868b9799f817a4adb22108d93c490d42f307f
Stijn Tintel <stijn@linux-ipv6.be>
2021-12-06 10:58:38 +0200

    Revert "Let's take a step back and print a simple string"
    Now that I've figured out some stuff, let's trying accessing the __hwcap
    variable again.
    
    This reverts commit 94f31d1406cd8ae5a0e99e2fc6d6f756fa4479c6.
    
    Signed-off-by: Stijn Tintel <stijn@linux-ipv6.be>

--- asm.s
+++ asm.s
@@ -10,8 +10,12 @@ doasm:
 
 	# need to skip .long because it's not a valid instruction
 	# is this the only way of loading in a variable?
+	# also why is this -. voodoo needed and where is it explained properly
 	bl	0f
-	.string "foo"
+	.long	__hwcap-.
+	# this emits difference between the address of __hwcap and the current address
+	# current address is directly after the bl, where the LR now points
+	# wat? welp
 
 0:
 	# read LR value into GPR4
@@ -21,7 +25,7 @@ doasm:
 	addi	0,0,4		# linux ppc write syscall number to GPR0
 	addi	3,0,1		# first argument (fd = 1) to GPR3
 				# second argument is already in GPR4 by mflr
-	addi	5,0,4		# third argument (len = 4 bytes) to GPR5
+	addi	5,0,1		# third argument (len = 1 byte) to GPR5
 	sc
 
 	# GPR14 to LR
